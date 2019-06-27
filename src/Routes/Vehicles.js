0727b47b5af163c24f9ea53d02bc9b45a3f8261d
RayapinA <rayapin.antoine36@gmail.com>
2019-06-27 16:17:18 +0200

    edit vehicle

--- src/Routes/Vehicles.js
+++ src/Routes/Vehicles.js
@@ -1,7 +1,10 @@
-import {allListVehicles} from '../Controller/Vehicles';
+import {allListVehicles, editVehicles} from '../Controller/Vehicles';
 import {rootApi} from '../config'
 
 export function routesVehicles(app, conn) {
     app.route(rootApi + '/vehicles/all')
         .get(allListVehicles(conn))
-}
+
+    app.route(rootApi + '/vehicles/edit/:id')
+        .patch(editVehicles(conn))
+}   
