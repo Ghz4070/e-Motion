e34a176ee633fc991a65d5895e461f9cd39d40c3
Alex <alex.rigueur@gmail.com>
2019-06-27 16:15:26 +0200

    add method for post offer

--- src/Controller/Offers.js
+++ src/Controller/Offers.js
@@ -2,7 +2,30 @@ export function getAllOffers(conn) {
     return (req, res) => {
         conn.query('SELECT * FROM offers')
         .then((result) => {
-            res.json(result)
+            res.json(result);
+            console.log(result)
+        })
+        .catch((err) => res.json(err.message))
+    }
+}
+
+export function getOfferById(conn) {
+    return (req, res) => {
+        conn.query('SELECT * FROM offers WHERE idoffers = ?', req.params.id)
+        .then((result) => {
+            res.json(result);
+            console.log(result)
+        })
+        .catch((err) => res.json(err.message))
+    }
+}
+
+export function postOffer(conn) {
+    return (req, res) => {
+        conn.query('INSERT INTO offers (title, price, description, penality, nbKm, pointFidelityOffers) VALUES (?,?,?,?,?,?)',
+        [req.body.title,req.body.price,req.body.description,req.body.penality,req.body.nbKm,req.body.pointFidelityOffers])
+        .then((result) => {
+            res.json(result);
             console.log(result)
         })
         .catch((err) => res.json(err.message))
