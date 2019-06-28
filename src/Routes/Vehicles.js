93eb678f04165c96567c66b203554547b994e68b
Ghz4070 <ghezal-ilies@outlook.fr>
2019-06-28 14:39:03 +0200

    edit routes and add method show offers with dates about id

--- src/Routes/Vehicles.js
+++ src/Routes/Vehicles.js
@@ -6,11 +6,11 @@ import {
     addVehicles,
     outVehiclesOfList
 } from '../Controller/Vehicles';
-import { rootApi } from '../config'
-import { checkToken } from '../middleware';
+import {rootApi} from '../config'
+import {checkToken} from '../middleware';
 
 export function routesVehicles(app, conn, ProtectedRoutes) {
-    ProtectedRoutes.use(checkToken)
+    ProtectedRoutes.use(checkToken);
 
     app.route(rootApi + '/vehicles/all')
         .get(allListVehicles(conn));
@@ -21,14 +21,14 @@ export function routesVehicles(app, conn, ProtectedRoutes) {
     app.route(rootApi + '/vehicles/:id')
         .get(getVehicleById(conn));
 
-    ProtectedRoutes.route(rootApi + '/vehicles/edit/:id')
+    ProtectedRoutes.route('/vehicles/edit/:id')
         .patch(editVehicles(conn));
-    
 
-    ProtectedRoutes.route(rootApi + '/vehicles/add')
+
+    ProtectedRoutes.route('/vehicles/add')
         .post(addVehicles(conn));
-    
-    ProtectedRoutes.route(rootApi + '/vehicles/delete/:id')
+
+    ProtectedRoutes.route('/vehicles/delete/:id')
         .delete(outVehiclesOfList(conn));
 
     app.use(rootApi, ProtectedRoutes);
