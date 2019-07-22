9ef7786838107b69412113eb4e816af98e0f45b5
Alex <alex.rigueur@gmail.com>
2019-07-22 12:02:30 +0200

    update

--- src/Controller/Offers.js
+++ src/Controller/Offers.js
@@ -58,39 +58,44 @@ export function removeOffer() {
 
 export function updateOffer() {
     return (req, res) => {
-        req.sql.query('SELECT * FROM offers WHERE idoffers= ?', req.params.id)
-            .then((result2) => {
-                let title = result2[0].title;
-                let price = result2[0].price;
-                let description = result2[0].description;
-                let penality = result2[0].penality;
-                let nbKm = result2[0].nbKm;
-                let pointFidelityOffers = result2[0].pointFidelityOffers;
+        if (decodeToken.role != "ROLE_ADMIN" || decodeToken.role != "ROLE_POPRIO") {
+            res.json(error(new Error("Can't not use this method").message));
+        }
+        else {
+            req.sql.query('SELECT * FROM offers WHERE idoffers= ?', req.params.id)
+                .then((result2) => {
+                    let title = result2[0].title;
+                    let price = result2[0].price;
+                    let description = result2[0].description;
+                    let penality = result2[0].penality;
+                    let nbKm = result2[0].nbKm;
+                    let pointFidelityOffers = result2[0].pointFidelityOffers;
 
-                if (req.body.title != undefined) {
-                    title = req.body.title
-                }
-                if (req.body.price != undefined) {
-                    price = req.body.price
-                }
-                if (req.body.description != undefined) {
-                    description = req.body.description
-                }
-                if (req.body.penality != undefined) {
-                    penality = req.body.penality
-                }
-                if (req.body.nbKm != undefined) {
-                    nbKm = req.body.nbKm
-                }
-                if (req.body.pointFidelityOffers != undefined) {
-                    pointFidelityOffers = req.body.pointFidelityOffers
-                }
-                req.sql.query('UPDATE offers SET title=?, price=?, description=?, penality=?, nbKm=?, pointFidelityOffers=? WHERE idoffers = ?',
-                    [title, price, description, penality, nbKm, pointFidelityOffers, req.params.id])
-                    .then((result) => {
-                        res.json(success(result));
-                    })
-                    .catch((err) => res.json(error(err.message)))
-            })
+                    if (req.body.title != undefined) {
+                        title = req.body.title
+                    }
+                    if (req.body.price != undefined) {
+                        price = req.body.price
+                    }
+                    if (req.body.description != undefined) {
+                        description = req.body.description
+                    }
+                    if (req.body.penality != undefined) {
+                        penality = req.body.penality
+                    }
+                    if (req.body.nbKm != undefined) {
+                        nbKm = req.body.nbKm
+                    }
+                    if (req.body.pointFidelityOffers != undefined) {
+                        pointFidelityOffers = req.body.pointFidelityOffers
+                    }
+                    req.sql.query('UPDATE offers SET title=?, price=?, description=?, penality=?, nbKm=?, pointFidelityOffers=? WHERE idoffers = ?',
+                        [title, price, description, penality, nbKm, pointFidelityOffers, req.params.id])
+                        .then((result) => {
+                            res.json(success(result));
+                        })
+                        .catch((err) => res.json(error(err.message)))
+                })
+        }
     }
 }
