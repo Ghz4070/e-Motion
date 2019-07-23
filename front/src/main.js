e5cea9b638334f9e54bf1f6d113772fa45672c0b
Alex <alex.rigueur@gmail.com>
2019-07-23 11:55:12 +0200

    udpate all theme

--- front/src/main.js
+++ front/src/main.js
@@ -1,16 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import BootstrapVue from 'bootstrap-vue'
+import Vue from 'vue';
+import App from './App.vue';
+// You can change this import to `import router from './starterRouter'` to quickly start development from a blank layout.
+import router from './router';
+import NowUiKit from './plugins/now-ui-kit';
 
-Vue.use(BootstrapVue)
+Vue.config.productionTip = false;
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.config.productionTip = false
+Vue.use(NowUiKit);
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
