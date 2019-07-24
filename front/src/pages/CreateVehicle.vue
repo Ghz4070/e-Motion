07960cfa09ed3886fb6ae3908a70d93181b3e649
Ghz4070 <ghezal-ilies@outlook.fr>
2019-07-24 08:47:15 +0200

    modification formulaire vehicule

--- front/src/pages/CreateVehicle.vue
+++ front/src/pages/CreateVehicle.vue
@@ -1,7 +1,32 @@
 <template>
-
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <card class="card-signup" header-classes="text-center" color="orange">
+                    <template slot="header">
+                        <fg-input
+                                class="no-border"
+                                placeholder="Marque"
+                                addon-left-icon="now-ui-icons text_caps-small"
+                                v-model="brand"
+                        >
+                        </fg-input>
+                    </template>
+                </card>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
+    import {Card, FormGroupInput, Button} from '@/components';
+
+    export default {
+        components: {
+            Card,
+            [Button.name]: Button,
+            [FormGroupInput.name]: FormGroupInput
+        }
+    }
 </script>
 <style>
 </style>
--- front/src/router.js
+++ front/src/router.js
@@ -12,67 +12,67 @@ import CreateVehicle from './pages/CreateVehicle.vue';
 Vue.use(Router);
 
 export default new Router({
-  linkExactActiveClass: 'active',
-  mode:'history',
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      components: { default: Index, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: 'black' }
-      }
-    },
-    {
-      path: '/landing',
-      name: 'landing',
-      components: { default: Landing, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: 'black' }
-      }
-    },
-    {
-      path: '/login',
-      name: 'login',
-      components: { default: Login, header: MainNavbar },
-      props: {
-        header: { colorOnScroll: 400 }
-      }
-    },
-    {
-      path: '/createVehicle',
-      name: 'createVehicle',
-      components: {default: CreateVehicle, header: MainNavbar, footer: MainFooter},
-      props: {
-        header: {colorOnScroll: 400},
-        footer: {backgroundColor: 'black'}
-      }
-    },
-    {
-      path: '/profile',
-      name: 'profile',
-      components: { default: Profile, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: 'black' }
-      }
-    },
-    {
-      path:'/signup',
-      name:'signup',
-      components: { default:Signup, header: MainNavbar},
-      props: {
-        header: { colorOnScroll: 400 }
-      }
-    },
-  ],
-  scrollBehavior: to => {
-    if (to.hash) {
-      return { selector: to.hash };
-    } else {
-      return { x: 0, y: 0 };
+    linkExactActiveClass: 'active',
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'index',
+            components: {default: Index, header: MainNavbar, footer: MainFooter},
+            props: {
+                header: {colorOnScroll: 400},
+                footer: {backgroundColor: 'black'}
+            }
+        },
+        {
+            path: '/landing',
+            name: 'landing',
+            components: {default: Landing, header: MainNavbar, footer: MainFooter},
+            props: {
+                header: {colorOnScroll: 400},
+                footer: {backgroundColor: 'black'}
+            }
+        },
+        {
+            path: '/login',
+            name: 'login',
+            components: {default: Login, header: MainNavbar},
+            props: {
+                header: {colorOnScroll: 400}
+            }
+        },
+        {
+            path: '/createVehicle',
+            name: 'createVehicle',
+            components: {default: CreateVehicle, header: MainNavbar, footer: MainFooter},
+            props: {
+                header: {colorOnScroll: 400},
+                footer: {backgroundColor: 'black'}
+            }
+        },
+        {
+            path: '/profile',
+            name: 'profile',
+            components: {default: Profile, header: MainNavbar, footer: MainFooter},
+            props: {
+                header: {colorOnScroll: 400},
+                footer: {backgroundColor: 'black'}
+            }
+        },
+        {
+            path: '/signup',
+            name: 'signup',
+            components: {default: Signup, header: MainNavbar},
+            props: {
+                header: {colorOnScroll: 400}
+            }
+        },
+    ],
+    scrollBehavior: to => {
+        if (to.hash) {
+            return {selector: to.hash};
+        } else {
+            return {x: 0, y: 0};
+        }
     }
-  }
 });
