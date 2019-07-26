4dcdecc2588ccf7481182b69683a3313d19db6e2
RayapinA <rayapin.antoine36@gmail.com>
2019-07-26 11:30:19 +0200

    Fonction AddLocation avec la nouvelle base

--- src/Controller/Locations.js
+++ src/Controller/Locations.js
@@ -5,17 +5,17 @@ export function addLocation() {
     return (req, res) => {
         const decodeTokenRole = JSON.parse(jwt.decode(req.headers['x-access-token']).role).role;
 
-        //if (decodeTokenRole.indexOf('ROLE_ADMIN') !== -1 || decodeTokenRole.indexOf('ROLE_POPRIO') !== -1) {
-                                                                    //, users_idusers, cancelLocation, pointFidelityUsed 
-            req.sql.query(' INSERT INTO location (startDate, endDate) VALUES (?, ?)',
-            [req.body.startDate, req.body.endDate]) //, req.body.users_idusers, req.body.cancelLocation, req.body.pointFidelityUsed]
+        if (decodeTokenRole.indexOf('ROLE_ADMIN') !== -1 || decodeTokenRole.indexOf('ROLE_POPRIO') !== -1) {
+                                                                    //
+            req.sql.query(' INSERT INTO location (startDate, endDate, users_idusers, cancelLocation, pointFidelityUsed ) VALUES (?, ?, ?, ?, ?)',
+            [req.body.startDate, req.body.endDate, req.body.users_idusers, req.body.cancelLocation, req.body.pointFidelityUsed]) 
                 .then((result) => {
                     res.json(success(result));
                 })
                 .catch((err) => res.json(error(err.message)));
-        //} else {
-          //  res.json(error(new Error("Can't not use this method").message));
-        //}
+        } else {
+            res.json(error(new Error("Can't not use this method").message));
+        }
     }
 }
 
