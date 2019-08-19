4118e8748c5debff9a4437bb3e2779f9994ec593
Alex <alex.rigueur@gmail.com>
2019-08-19 21:58:09 +0200

    add payment with stripe and update fidelity offer

--- src/Method/methodUser.js
+++ src/Method/methodUser.js
@@ -2,12 +2,18 @@
 export function reduc (pointFidelity ,priceOffers) {
     if(pointFidelity >= 100 && pointFidelity <= 199){
         return priceOffers -(priceOffers*0.05) 
-    }else if(pointFidelity >=200 && pointFidelity <= 499){
+    }else if(pointFidelity >=200 && pointFidelity <= 299){
         return priceOffers - (priceOffers*0.10) 
-    }else if(pointFidelity >= 500 && pointFidelity <= 999){
+    }else if(pointFidelity >= 300 && pointFidelity <= 399){
         return priceOffers -(priceOffers*0.15) 
-    }else if(pointFidelity >= 1000){
+    }else if(pointFidelity >= 400 && pointFidelity <= 499){
         return priceOffers -(priceOffers*0.20) 
+    }else if(pointFidelity >= 500 && pointFidelity <= 599){
+        return priceOffers -(priceOffers*0.25) 
+    }else if(pointFidelity >= 599 && pointFidelity <= 600){
+        return priceOffers -(priceOffers*0.30) 
+    }else if(pointFidelity > 600){
+        return priceOffers -(priceOffers*0.30) 
     }else if(pointFidelity<100) {
         return priceOffers
     }else{
